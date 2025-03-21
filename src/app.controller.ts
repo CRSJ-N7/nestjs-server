4ca5a3cd0846b1e056e70eb26a532dcfe635e8e8
crossjawz@gmail.com <crossjawz@gmail.com>
2025-03-21 15:36:19 +0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('about')
+  getTestString(): string {
+    return this.appService.getTestString();
+  }
+
+  @Get('shiet')
+  getSomeNewShit(): string {
+    return this.appService.getSomeNewShit();
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello, this is my NEST server! (c) CRSJ!';
+  }
+  getTestString(): string {
+    return 'Hello, this is some new string!';
+  }
+
+  getSomeNewShit(): string {
+    return crypto.randomUUID();
+  }
+}
